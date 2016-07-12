5bf32f77e5646da4bbecb349b3ea3d6a258de5f9
akitotakeki <takeki@hal.t.u-tokyo.ac.jp>
2016-07-12 20:21:54 +0900

    refine creat_resnet

--- create_resnet.py
+++ create_resnet.py
@@ -1,53 +1,112 @@
+from __future__ import print_function
 import caffe
-from caffe import layers as L
-from caffe import params as P
+from caffe import layers as L, params as P, to_proto
+from caffe.proto import caffe_pb2
 
-def resnet(lmdb, batch_size, deploy=False):
+def conv_factory(bottom, ks, nout, stride=1, pad=0):
+    conv = L.Convolution(bottom, kernel_size=ks, stride=stride,
+                                num_output=nout, pad=pad, bias_term=False, weight_filler=dict(type='msra'))
+    batch_norm = L.BatchNorm(conv, in_place=True, param=[dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)])
+    scale = L.Scale(batch_norm, bias_term=True, in_place=True)
+    relu = L.ReLU(scale, in_place=True)
+    return relu
+
+def conv_factory_inverse(bottom, ks, nout, stride=1, pad=0):
+    batch_norm = L.BatchNorm(bottom, in_place=True, param=[dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)])
+    scale = L.Scale(batch_norm, bias_term=True, in_place=True)
+    relu = L.ReLU(scale, in_place=True)
+    conv = L.Convolution(relu, kernel_size=ks, stride=stride,
+                                num_output=nout, pad=pad, weight_filler=dict(type='msra'))
+    return conv
+
+def conv_factory_inverse_no_inplace(bottom, ks, nout, stride=1, pad=0):
+    batch_norm = L.BatchNorm(bottom, in_place=False, param=[dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)])
+    scale = L.Scale(batch_norm, bias_term=True, in_place=True)
+    relu = L.ReLU(scale, in_place=True)
+    conv = L.Convolution(relu, kernel_size=ks, stride=stride,
+                                num_output=nout, pad=pad, weight_filler=dict(type='msra'))
+    return conv
+
+def residual_factory1(bottom, num_filter):
+    conv1 = conv_factory_inverse_no_inplace(bottom, 3, num_filter, 1, 1);
+    conv2 = conv_factory_inverse(conv1, 3, num_filter, 1, 1);
+    addition = L.Eltwise(bottom, conv2, operation=P.Eltwise.SUM)
+    return addition
+
+def residual_factory_proj(bottom, num_filter, stride=2):
+    batch_norm = L.BatchNorm(bottom, in_place=True, param=[dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)])
+    scale = L.Scale(batch_norm, bias_term=True, in_place=True)
+    conv1 = conv_factory(scale, 3, num_filter, stride, 1);
+    conv2 = L.Convolution(conv1, kernel_size=3, stride=1,
+                                num_output=num_filter, pad=1, weight_filler=dict(type='msra'));
+    proj = L.Convolution(scale, kernel_size=1, stride=stride,
+                                num_output=num_filter, pad=0, weight_filler=dict(type='msra'));
+    addition = L.Eltwise(conv2, proj, operation=P.Eltwise.SUM)
+    return addition
+
+def max_pool(bottom, ks, stride=1):
+    return L.Pooling(bottom, pool=P.Pooling.MAX, kernel_size=ks, stride=stride)
+
+def resnet(lmdb, batch_size=256, stages=[2, 2, 2, 2], first_output=64, deploy=False):
 #it is tricky to produce the deploy prototxt file, as the data input is not from a layer, so we have to creat a workaround
 #producing training and testing prototxt files is pretty straight forward
-  n = caffe.NetSpec()
+  #n = caffe.NetSpec()
   if deploy==False:
-    n.data, n.label = L.Data(batch_size=batch_size, backend=P.Data.LMDB, source=lmdb, ntop=2)
+    data, label = L.Data(source=lmdb, backend=P.Data.LMDB, batch_size=batch_size, ntop=2,
+            transform_param=dict(crop_size=224, mean_value=[104, 117, 123], mirror=True))
   # produce data definition for deploy net
   else:
     input="data"
     dim1=1
     dim2=1
-    dim3=64
+    dim3=32
     dim4=32
     #make an empty "data" layer so the next layer accepting input will be able to take the correct blob name "data",
     #we will later have to remove this layer from the serialization string, since this is just a placeholder
-    n.data=L.Layer()
+    data=L.Layer()
 
-  n.conv2 = L.Convolution(n.data, kernel_h=6, kernel_w=1, num_output=8, weight_filler=dict(type='xavier'))
-  n.pool1 = L.Pooling(n.conv2, kernel_h=3, kernel_w=1, stride_h=2, stride_w=1, pool=P.Pooling.MAX)
+  relu1 = conv_factory(data, 7, first_output, stride=2, pad=3)
+  residual = max_pool(relu1, 3, stride=2)
 
-  n.drop2=L.Dropout(n.pool1,dropout_ratio=0.1)
-  n.ip1=L.InnerProduct(n.drop2, num_output=196, weight_filler=dict(type='xavier'))
+  k=0
+  for i in stages:
+#    first_output *= 2
+    for j in range(i):
+      if j==0:
+        if k==0:
+          residual = residual_factory_proj(residual, first_output, 1)
+          k+=1
+        else:
+          residual = residual_factory_proj(residual, first_output, 2)
+      else:
+        residual = residual_factory1(residual, first_output)
+    first_output *= 2
 
-  n.relu1 = L.ELU(n.ip1, in_place=True, alpha=1)
-  n.ip4 = L.InnerProduct(n.relu1, num_output=12, weight_filler=dict(type='xavier'))
+  glb_pool = L.Pooling(residual, pool=P.Pooling.AVE, global_pooling=True);
+  fc = L.InnerProduct(glb_pool, num_output=1000, weight_filler=dict(type='xavier'))
 
   #n.loss layer is only in training and testing nets, but not in deploy net.
   if deploy==False:
-    n.loss = L.SoftmaxWithLoss(n.ip4, n.label)
-    return str(n.to_proto())
+    loss = L.SoftmaxWithLoss(fc, label)
+    return to_proto(loss)
   #for generating the deploy net
-  else:
+ # else:
     #generate the input information header string
-    deploy_str='input: {}\ninput_dim: {}\ninput_dim: {}\ninput_dim: {}\ninput_dim: {}'.format('"'+input+'"', dim1, dim2, dim3, dim4)
+#    deploy_str='input: {}\ninput_dim: {}\ninput_dim: {}\ninput_dim: {}\ninput_dim: {}'.format('"'+input+'"', dim1, dim2, dim3, dim4)
     #assemble the input header with the net layers string.  remove the first placeholder layer from the net string.
-    return deploy_str+'\n'+'layer {'+'layer {'.join(str(n.to_proto()).split('layer {')[2:])
+#    return deploy_str+'\n'+'layer {'+'layer {'.join(to_proto(loss).split('layer {')[2:])
 
-#write the net prototxt files out
-with open('ResNet_train.prototxt', 'w') as f:
-  print 'writing train'
-  f.write(resnet('/srv/dataset/train_lmdb', 100))
+def make_net():
+  with open('resnet_train.prototxt', 'w') as f:
+#    print(writing train)
+    print(resnet('/srv/datasets/ILSVRC2012/train_lmdb'), file=f)
+  with open('resnet_test.prototxt', 'w') as f:
+#    print(writing test)
+    print(resnet('/srv/dataset/ILSVRC2012/test_lmdb'), file=f)
+#  with open('resnet_deploy.prototxt', 'w') as f:
+#    print(writing deploy)
+#    print(resnet('srv/dataset/train_lmdb', deploy=True), file=f)
 
-with open('ResNet_test.prototxt', 'w') as f:
-  print 'writing test'
-  f.write(resnet('/srv/dataset/test_lmdb', 100))
-
-with open('ResNet_deploy.prototxt', 'w') as f:
-  print 'writing deploy'
-  f.write(str(resnet('', 0, deploy=True)))
+if __name__ == '__main__':
+  make_net()
+  caffe.Net('resnet_train.prototxt', caffe.TRAIN)
